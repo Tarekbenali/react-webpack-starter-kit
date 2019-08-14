45f3a9f603a0da0452b6dd09f30c46afb17d7d44
Ben Ali Tarek <benalitarek90@gmail.com>
2019-08-14 09:24:43 +0100

    Add Files to github

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+  return (
+      <div>
+       <h2 className="h2style">Test Page </h2> 
+       <h2 className="h2style">Test Page </h2> 
+       <h2>Test Page </h2> 
+      </div>
+  );
+};
+
+export default App;
